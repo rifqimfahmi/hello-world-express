420d172c6c1f38ec4778d8cc8a97f4a7a4675a26
rifqimfahmi <rifqi416@gmail.com>
2023-03-26 21:50:43 +0700

    Add round-robin load balancer example

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
 
 app.get("/", (req,res) => {
     console.log(req.headers)
-    console.log("log after header")
+    console.log(`Hello port ${port}`)
     res.send("hello world")
 })
 
